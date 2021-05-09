ca8eab247660d882f5b3bf0f951f006f3a6d7678
Tauros <admin@tauros.finance>
2021-05-10 01:54:08 +0700

    Initial commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,156 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://dex.pantherswap.com",
+      },
+      {
+        label: "Liquidity",
+        href: "https://dex.pantherswap.com/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://pancakeswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://pancakeswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://pancakeswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://pancakeswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Voting",
+        href: "https://voting.pantherswap.com",
+      },
+      {
+        label: "Github",
+        href: "https://github.com/pancakeswap",
+      },
+      {
+        label: "Docs",
+        href: "https://docs.pantherswap.com",
+      },
+      {
+        label: "Blog",
+        href: "https://pancakeswap.medium.com",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Github",
+    icon: "GithubIcon",
+    href: "https://github.com/pantherswap",
+  },
+  {
+    label: "Docs",
+    icon: "GitbookIcon",
+    href: "https://docs.pantherswap.com",
+  },
+  {
+    label: "Medium",
+    icon: "MediumIcon",
+    href: "https://pantherswap.medium.com",
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/PantherSwap",
+  },
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/PantherSwap",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/PantherSwapAnn",
+      },
+    ],
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
